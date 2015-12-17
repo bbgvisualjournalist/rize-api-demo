86f75bad85d56e84d3ffe3adc051f91ebc3e2d5a
Brian Williamson <brianwilliamson@Brians-MacBook-Pro.local>
2015-12-17 11:20:36 -0500

    Removing reference to routes/users.

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 
 var config = require('./config')
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
